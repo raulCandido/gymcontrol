3f5447c0d1472f7363bfa70c6f46d66a141ac377
Raul Candido <raul.cand@hotmail.com>
2021-08-13 16:43:14 -0300

    commit inicial

---
+++ src/main/java/br/com/gym/gymcontrol/GymcontrolApplication.java
@@ -0,0 +1,13 @@
+package br.com.gym.gymcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gym/gymcontrol/GymcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gym.gymcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
